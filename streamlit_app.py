269e6284d7d424a79d9bfef817f297116042ea5b
Harikrishna V <121329487+harikrishnasf@users.noreply.github.com>
2022-12-25 00:13:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
 import pandas
+import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,5 +26,5 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Avocado')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
