38a53c2bdea8a5e51a1134dccbb6cd825db07f15
Harikrishna V <121329487+harikrishnasf@users.noreply.github.com>
2022-12-25 17:05:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,13 @@ streamlit.text('🥣 Omega 3 & Blueberry Oat Meal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
-
-
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
-
 streamlit.header('Fruityvice Fruit Advice!')
 try:
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -36,7 +29,6 @@ try:
 #create the repeatable code block(called function)
  def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#fruityvice_response = requests.get('https://fruityvice.com/api/fruit/apple')
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized)
   return fruityvice_normalized
@@ -44,22 +36,14 @@ try:
 except URLError as e:
   streamlit.error()
 #streamlit.stop()
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("The frui load list contains:")
-#streamlit.text(my_data_row)
 my_data_rows = my_cur.fetchall()
 streamlit.header('The fruit load list contains: ')
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
